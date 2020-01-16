46e4cf6ef67d34add4ec2052327c64f98baab032
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-16 18:28:23 -0500

    add in support to find '.test.js' files and their path and store reference to each

--- runner.js
+++ runner.js
@@ -3,13 +3,27 @@ const path = require('path');
 
 class Runner {
   constructor() {
-    this.file = [];
+    this.testFiles = [];
   }
 
   async collectFiles(targetPath) {
     const files = await fs.promises.readdir(targetPath);
 
-    return files;
+    for (let file of files) {
+      const filepath = path.join(targetPath, file);
+
+      //   console.log(`filepath of current file ${file} is: ${filepath}`);
+
+      const stats = await fs.promises.lstat(filepath);
+
+      if (stats.isFile() && file.includes('.test.js')) {
+        this.testFiles.push({ name: filepath });
+      } else if (stats.isDirectory()) {
+        const childFiles = await fs.promises.readdir(filepath);
+
+        files.push(...childFiles.map(f => path.join(file, f)));
+      }
+    }
   }
 }
 
