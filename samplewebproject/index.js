43acd56dd192f64fc2b45f55566f9068daac8685
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-17 12:02:44 -0500

    simple browser based app for testing purposes via jsdom

---
+++ samplewebproject/index.js
@@ -0,0 +1,11 @@
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { value } = document.querySelector('input');
+  const header = document.querySelector('h1');
+  if (value.includes('@')) {
+    header.innerHTML = 'looks good';
+  } else {
+    header.innerHTML = 'invalid email';
+  }
+});
