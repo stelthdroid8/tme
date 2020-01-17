85e9bb27112fa15eb901e0cbbf1e4367897d8640
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-16 19:37:27 -0500

    more testing

--- sampleproject/test/forEach.test.js
+++ sampleproject/test/forEach.test.js
@@ -1,8 +1,12 @@
 const { forEach } = require('../index');
 const assert = require('assert');
 
+let numbers;
+beforeEach(() => {
+  numbers = [1, 2, 3];
+});
 it('should sum an array', () => {
-  const numbers = [1, 2, 3];
+  //   const numbers = [1, 2, 3];
 
   let total = 0;
 
@@ -11,4 +15,12 @@ it('should sum an array', () => {
   });
 
   assert.strictEqual(total, 6);
+  numbers.push(3);
+  numbers.push(3);
+  numbers.push(3);
+  numbers.push(3);
+});
+
+it('beforeEach is ran each time', () => {
+  assert.strictEqual(numbers.length, 5);
 });
